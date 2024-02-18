4d51d76b73e06739b4afc93792494c2ee61cce8f
wzwmzm <wzw_mzm@aliyun.com>
2024-02-18 13:32:18 +0800

    a

---
+++ test1/target/release/deps/test1-f2bfabbbc461d6da.d
@@ -0,0 +1,5 @@
+/home/wzw/project/rust/test1/target/release/deps/test1-f2bfabbbc461d6da: src/main.rs
+
+/home/wzw/project/rust/test1/target/release/deps/test1-f2bfabbbc461d6da.d: src/main.rs
+
+src/main.rs:
---
+++ test1/target/release/test1.d
@@ -0,0 +1,1 @@
+/home/wzw/project/rust/test1/target/release/test1: /home/wzw/project/rust/test1/src/main.rs
